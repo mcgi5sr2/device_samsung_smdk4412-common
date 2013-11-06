70f498561c89f886e01bd7c39ec1e6746c3817eb
xplodwild <xplod@ouverta.fr>
2013-11-06 19:17:49 +0100

    common: Disable GL preload
    Change-Id: I91ee3b8f08344fe6bba061540ded28a7fd4df80f

--- common.mk
+++ common.mk
@@ -153,7 +153,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=131072 \
     debug.hwui.render_dirty_regions=false \
-    ro.bq.gpu_to_cpu_unsupported=1
+    ro.bq.gpu_to_cpu_unsupported=1 \
+    ro.zygote.disable_gl_preload=true
 
 PRODUCT_TAGS += dalvik.gc.type-precise
 
