456fa0d743142870fc883ed8aae2356ee96937ed
Ben Edmunds <sammybenblue2@gmail.com>
2014-07-21 01:02:00 +0530

    Add audio.r_submix.default for casting
    Change-Id: Ia4c0022881b1850d85459a7117e71b8048a0a55b

--- common.mk
+++ common.mk
@@ -68,6 +68,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES := \
     audio.a2dp.default \
     audio.primary.smdk4x12 \
+    audio.r_submix.default \
     audio.usb.default \
     com.android.future.usb.accessory \
     gralloc.exynos4 \
